ba3c4aed7ea451dac3499b3c398d72037a22d385
vildan-kharisov <vildan-kharisov@yandex.ru>
2026-05-11 17:14:59 +0300

    my dbt project

---
+++ models/staging/flights/_flights__docs.md
@@ -0,0 +1,10 @@
+{% docs aircafts_description %}
+
+# Таблица с моделями самолетов
+Содержит данные, полученные запросом
+```sql
+select *
+from demo.bookings.aircrafts
+```
+
+{% enddocs %}
---
+++ models/staging/flights/stg_flights__airports.sql
@@ -0,0 +1,16 @@
+{{
+      config(
+        materialized = 'table'
+        )   
+}}
+ 
+select
+  airport_code,
+  airport_name,
+  city,
+  coordinates,
+  timezone
+
+from {{ source('demo_src', 'airports') }}
+
+    
