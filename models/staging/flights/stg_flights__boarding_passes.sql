0faf79c6d5e88506809f8df1cf843e82e071066f
vildan-kharisov <vildan-kharisov@yandex.ru>
2026-05-15 09:32:23 +0300

    update

--- models/staging/flights/stg_flights__boarding_passes.sql
+++ models/staging/flights/stg_flights__boarding_passes.sql
@@ -1,26 +1,12 @@
 {{
-      config(
-        materialized = 'table'
-        )   
+config(
+    materialized = 'table'
+)
 }}
-
 select
-    tf.ticket_no as ticket_no,
-    tf.flight_id as flight_id,
-    tf.fare_conditions as fare_conditions,
-    tf.amount as amount,
-    (select case
-        when bp.boarding_no is null 
-        then 'no' 
-        else 'yes' 
-    end) as boarding_pass_exists,
-    bp.boarding_no as boarding_no,
-    bp.seat_no as seat_no,
-    current_date as load_date
+    ticket_no,
+    flight_id,
+    boarding_no,
+    seat_no
 from
-    {{ ref('stg_flights__ticket_flights') }} tf
-        left join {{ ref('stg_flights__boarding_passes') }} bp
-        on tf.ticket_no=bp.ticket_no
-        and tf.flight_id=bp.flight_id
-
-    
+    {{ source('demo_src', 'boarding_passes') }}
