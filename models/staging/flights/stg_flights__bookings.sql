a72315af5f1395aeed1fbcfd74b5f0651a3d70b4
vildan-kharisov <vildan-kharisov@yandex.ru>
2026-05-15 07:52:58 +0300

    update

--- models/staging/flights/stg_flights__bookings.sql
+++ models/staging/flights/stg_flights__bookings.sql
@@ -1,12 +1,21 @@
 {{
       config(
-        materialized = 'table'
+        materialized = 'table',
+        on_configuration_change = "apply",
+        indexes = [
+          {
+            "columns":["book_date"],
+            "unique": false
+          }
+        ]
+
         )   
 }}
 select
 book_ref,
 book_date,
 total_amount
+
 from {{ source('demo_src', 'bookings') }}
 
     
