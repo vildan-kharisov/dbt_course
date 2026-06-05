2c5682c1e337e4c4b629dbc97db284d941b7b571
vildan-kharisov <vildan-kharisov@yandex.ru>
2026-06-05 12:49:44 +0300

    add

--- macros/my_utils.sql
+++ macros/my_utils.sql
@@ -111,3 +111,28 @@ SELECT NULL
 {%- endfor -%}
   
 {% endmacro %}
+
+
+{%- macro show_columns_relation_base(model_name) %}
+{% if execute %}
+
+{% set columns_query %}
+    select
+        COLUMN_NAME
+    from 
+        {{ target.database }}.INFORMATION_SCHEMA.COLUMNS 
+    where 
+        TABLE_SCHEMA = '{{ target.schema }}'  
+        AND TABLE_NAME = '{{ model_name }}'
+{% endset %}
+
+{% do log(columns_query, True) %}
+
+{% set columns = run_query(columns_query).columns[0].values() %}
+
+    {%- for column in columns -%}
+        {{ column }} {% if not loop.last -%}, {% endif %}
+    {%- endfor %}
+
+{% endif %}
+{% endmacro %}
