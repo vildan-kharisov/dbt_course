f02f7c4457197898879d6f563ecf57a4c0cccdd2
vildan-kharisov <vildan-kharisov@yandex.ru>
2026-06-10 09:15:42 +0300

    utils

--- models/intermediate/flights/fct_tickets.sql
+++ models/intermediate/flights/fct_tickets.sql
@@ -12,7 +12,7 @@ select
 from {{ ref('stg_flights__tickets') }}
 where passenger_id not in(select passenger_id from {{ ref('job_passengers') }} )
 
-{% set get_column = dbt_utils.get_column_values(table=ref('fct_tickets'), column='book_ref') %}
+{% set get_column = dbt_utils.get_column_values(table=this, column='book_ref') %}
 
 {% for col in get_column %}
   --{{col}}
