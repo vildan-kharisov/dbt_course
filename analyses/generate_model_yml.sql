b6846288b3baad5d314c51ff76e5674300a47977
vildan-kharisov <vildan-kharisov@yandex.ru>
2026-06-12 12:39:09 +0300

    codegen

--- analyses/generate_model_yml.sql
+++ analyses/generate_model_yml.sql
@@ -2,7 +2,15 @@
     model_names=['fct_bookings','fct_flights']
 ) }} #}
 
-{{ codegen.create_base_models(
+{# 1 (2/3). С помощью codegen.create_base_models создать код моделей, получащих данных из таблиц на источнике: flights и ticket_flights.
+
+2 (2/3). Сгенерируйте код yml файла по созданным моделям с помощью codegen.generate_model_yaml. #}
+
+{# {{ codegen.create_base_models(
     source_name="demo_src", 
     tables=['flights','ticket_flights']
+) }} #}
+
+{{ codegen.generate_model_yaml(
+    model_names=['stg_demo_src__flights','stg_demo_src__ticket_flights']
 ) }}
